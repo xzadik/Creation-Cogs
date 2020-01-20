3f8d42d306b6f690f7466e377f1f5b737bb91968
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-01-20 03:33:47 -0500

    Update uwu.py

--- uwu/uwu.py
+++ uwu/uwu.py
@@ -251,7 +251,6 @@ class uwu(commands.Cog, IDConverter):
     async def pat(self, ctx, member: discord.Member.mention):
         fauth = ctx.message.author.id
         auth = f'<@!{fauth}>'
-        member = <@ID>
         msg = rand(self.patm)
         if member == auth:
             return await ctx.send("A-are you feeling lonely? ;c")
